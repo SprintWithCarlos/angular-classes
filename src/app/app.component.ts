aa87da7c2113ee14244e0e7f121a77f0ab3fadad
Carlos Marchena <sprintwithcarlos@pm.me>
2019-01-28 00:59:09 +0100

    create module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  template: `
+    <h1>Hola Caracola</h1>
+    <p>Saludos</p>
+  `,
+  styles: ["h1 { font-weight: normal;}", "p {color: purple;}"]
 })
 export class AppComponent {
-  title = 'app';
+  title = "app";
 }
